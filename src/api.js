b041f916ad5d8a6e490a41b887bca5666e8a541b
sgrdev <95933073+sgrdev@users.noreply.github.com>
2023-06-30 12:40:11 +0545

    intregrated-city-search-api

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9df1068745msh172e1bd23a534a3p1cef9djsnfb33550deacf',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
